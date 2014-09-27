da235e0d8c58a6445e933bed5cd69a9d8dfada5c
Ieong David <ieong.david.id@gmail.com>
2014-09-27 19:13:53 +0200

    Carnet
    First Commit all project

---
+++ src/controller/LoginServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+	
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		if (name.equals("caca") && password.equals("caca")) {
+		out.println( "<h2>Hello, " + name + ", your password is \""
+				+ password + "\"</body></html>");
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+}
