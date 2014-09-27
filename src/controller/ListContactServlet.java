8f26b57828e5a636b8a71bdfe377cde98f9ef7ef
David San <davidsanfr@gmail.com>
2014-09-27 20:19:18 +0200

    list contact servlet

---
+++ src/controller/ListContactServlet.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Contact;
+import domain.ContactService;
+
+/**
+ * Servlet implementation class ListContactServlet
+ */
+public class ListContactServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ListContactServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		ContactService cs = new ContactService();
+		List<Contact> contacts = cs.listContact();
+		if (contacts != null) {
+			for (Contact c : contacts) {
+				response.getWriter().print(
+						String.format(" ", c.getId(), c.getFirstName(),
+								c.getLastName(), c.getEmail()));
+				response.getWriter().print("\n");
+			}
+		} else {
+			response.getWriter().print(
+					"Mille milliards de mille millions de mille sabords	! j'ai pas pu lister les contacts");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
